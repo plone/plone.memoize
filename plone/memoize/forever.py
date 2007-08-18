a666dbff5c56bb46cd72f2fba36ab6ab03b9b35c
Daniel Nouri <daniel.nouri@gmail.com>
2007-08-18 22:04:14 +0000

    Simplify forever by reuse of stuff from plone.memoize.volatile
    svn path=/plone.memoize/trunk/; revision=16801

--- plone/memoize/forever.py
+++ plone/memoize/forever.py
@@ -5,41 +5,15 @@ process lives.
 Stores values in a module-level variable.
 """
 
-_marker = object()
-_memos = {} # dict of dotted names to dict of cached values
+from plone.memoize import volatile
 
-class ForeverMemo(object):
-    
-    def memoize(self, func):
-        def memogetter(*args, **kwargs):
-            global _memos
-            
-            object_key = self.object_key(func)
-            cache = _memos.setdefault(object_key, {})
-            
-            # XXX this could be improved to unfold unhashables
-            # and optimized with pyrex
+_memos = {}
 
-            key = hash((args, frozenset(kwargs.items()),))
-            val = cache.get(key, _marker)
-            
-            if val is _marker:
-                val=func(*args, **kwargs)
-                cache[key]=val
-            return val
-        return memogetter
-        
-    def object_key(self, func):
-        method = hasattr(func, 'im_class')
-        if method:
-            return "%s.%s.%s" % (func.__module__, func.im_class.__name__, func.__name__,)
-        else:
-            return "%s.%s" % (func.__module__, func.__name__,)
+def memoize(fun):
+    def get_key(fun, *args, **kwargs):
+        return hash((args, frozenset(kwargs.items()),))
+    def get_cache(fun, *args, **kwargs):
+        return _memos
+    return volatile.cache(get_key, get_cache)(fun)
 
-_m = ForeverMemo()
-memoize = _m.memoize
-
-def memoizedproperty(func):
-    return property(_m.memoize(func))
-
-__all__ = (memoize, memoizedproperty)
+__all__ = (memoize,)
