cf13245348a3a7b76e08c63d5d4fdb94801a3da6
Hanno Schlichting <hanno@hannosch.eu>
2008-03-27 20:33:03 +0000

    Merged c20093 and c20093 from 1.0 branch
    svn path=/plone.memoize/trunk/; revision=20096

--- README.txt
+++ README.txt
@@ -1,7 +1,12 @@
-plone.memoize Package Readme
-=========================
+plone memoization decorators
+============================
 
-Overview
---------
+plone.memoize provides Python function decorators for caching the
+values of functions and methods.
 
-Decorator to memoize properties bound to a request
+The type of cache storage is freely configurable by the user, as is
+the cache key, which is what the function's value depends on.
+
+plone.memoize has support for memcached and is easily extended to use
+other caching storages.  It also has specialized decorators for use
+with Zope views.  However, plone.memoize can be used without Zope.
--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0.2'
+version = '1.1'
 
 setup(name='plone.memoize',
       version=version,
       description="Decorators for caching the values of functions and methods",
-      long_description="""\
-plone.memoize provides Python function decorators for caching the
-values of functions and methods.
-
-The type of cache storage is freely configurable by the user, as is
-the cache key, which is what the function's value depends on.
-
-plone.memoize has support for memcached and is easily extended to use
-other caching storages.  It also has specialized decorators for use
-with Zope views.  However, plone.memoize can be used without Zope.
-""",
+      long_description=open("README.txt").read() + \
+                       open("docs/HISTORY.txt").read(),
       classifiers=[],
       keywords='plone memoize decorator cache',
       author='Martin Aspeli, David "Whit" Morriss and Daniel Nouri',
