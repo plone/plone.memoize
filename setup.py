fcddb3f52c23be1d48dc7c48c5b86327d53c98be
Hanno Schlichting <hanno@hannosch.eu>
2008-03-31 17:02:05 +0000

    Merged c20207-20213 from the 1.0 branch into trunk
    svn path=/plone.memoize/trunk/; revision=20214

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '1.1'
 
 setup(name='plone.memoize',
       version=version,
       description="Decorators for caching the values of functions and methods",
-      long_description=open("README.txt").read() + \
+      long_description=open("README.txt").read() + '\n' + \
                        open("docs/HISTORY.txt").read(),
       classifiers=[],
       keywords='plone memoize decorator cache',
       author='Martin Aspeli, David "Whit" Morriss and Daniel Nouri',
       author_email='optilude@gmx.net',
       url='http://svn.plone.org/svn/plone/plone.memoize',
-      license='LGPL',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
       include_package_data=True,
