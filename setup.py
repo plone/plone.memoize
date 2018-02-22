ceafeffbd88a31ebed0283d0f1665ff0b36727cc
Maurits van Rees <maurits@vanrees.org>
2018-02-23 00:18:07 +0100

    Preparing release 1.2.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '1.2.2.dev0'
+version = '1.2.2'
 
 long_description = u"\n".join([
     read('README.rst'),
