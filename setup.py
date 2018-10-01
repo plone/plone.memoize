1ec5fe895a77f089e210a6bd637b384bdc48d32c
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:40:03 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     keywords='plone memoize decorator cache',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.memoize',
+    url='https://pypi.org/project/plone.memoize',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone'],
